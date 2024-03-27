ca66a49a824c4fa1702d93360d565aba66b789ac
lu1z1nn <fellipebezerra@hotmail.com>
2024-03-27 19:21:18 -0300

    nova atualização

---
+++ script.js
@@ -0,0 +1,9 @@
+const contain = document.querySelector(".container")
+const card = document.querySelectorAll(".text h1")
+
+
+function clickCard() {
+    console.log("funcionou")
+}
+
+card.addEventListener("click", clickCard)
